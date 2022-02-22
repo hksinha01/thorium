f66931902705480a1024c4f4c902799a215e47e2
hksinha01 <hksinha19012001@gmail.com>
2022-02-22 11:45:54 +0530

    assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ let obj = require('../logger/logger.js')
 const help = require("../util/helper.js")
 const format = require("../validator/formatter.js")
 const express = require('express');
+const lodash = require('lodash')
 
 const router = express.Router();
 
@@ -15,6 +16,29 @@ router.get('/test-me', function (req, res) {
     format.trim("  FunctionUP   ")
     format.lower("FunctionUp")
     format.upper("FunctionUp")
+
+    let months =['Jan','Feb','March','April','May','June','July','August','September','October','November','December']
+    let subarrays = lodash.chunk(months,3)
+    console.log("chunks of months : ", subarrays)
+
+    let odd = [1,3,5,7,9,11,13,15,17,19]
+    let last = lodash.tail(odd)
+    console.log(last)
+
+    let arr1 = [1,2,3]
+    let arr2 = [2,3,4]
+    let arr3 = [3,4,5]
+    let arr4 = [4,5,6]
+    let arr5 = [5,6,7]
+    let uni = lodash.union(arr1,arr2,arr3,arr4,arr5)  
+    console.log(uni)  
+
+    let m1 = ['horror','The Shining']
+    let m2 = ['drama','Titanic']
+    let m3 = ['thriller','Shutter Island']
+    let m4 = ['fantasy','Pans Labyrinth']
+    let movie = lodash.fromPairs([m1,m2,m3,m4])
+    console.log("Movie object :",movie)    
 
     res.send('hi,this is My first ever api!')
 });
