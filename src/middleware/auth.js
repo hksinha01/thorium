57a3816dd7f6d39e597d98c753215268e1b6f8eb
hksinha01 <98683627+hksinha01@users.noreply.github.com>
2022-03-11 14:46:07 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,29 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken")
+const mid = function(req, req, next) {
     //check the token in request header
     //validate this token
+    try{
+        let token = req.headers["x-auth-token"]
+        if(!token)
+        return res.status(401).send({status: false, msg:"Token not present"})
+    
+        let decodedToken = jwt.verify(token,"functionup-thorium")
+        if(!decodedToken)
+        return res.status(401).send({status:false,msg:"Token is invalid"})
+    
+        let userId = req.params.userId
+        let userLoggedIn = decodedToken.userId
+    
+        if(userId !== userLoggedIn ){
+            res.status(403).send({status : false, msg : "User is not Allowed to modify the request"})
+        } next()
+    }
+        catch(error)
+        {
+            res.status(500).send({msg : "eRROR",error : error.message})
+        }
 
-    next()
+    
 }
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports.mid=mid
